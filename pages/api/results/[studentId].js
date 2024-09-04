fcaad36f44efb93453aed8265e734f464c10b0dd
HIMANSHU RAWAT <rhimanshu828@gmail.com>
2024-09-04 18:11:36 +0530

    complete backend without excel upload

---
+++ pages/api/results/[studentId].js
@@ -0,0 +1,57 @@
+import { connectToDatabase } from "@/lib/db";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    res.status(401).json({ message: "Not authorized!" });
+    return;
+  }
+
+  const { studentId } = req.query;
+
+  if (!studentId || studentId.length === 0 || studentId.trim().length === 0) {
+    res.status(402).json({ message: "Student Id is required." });
+    return;
+  }
+
+  const client = await connectToDatabase();
+  const db = client.db();
+
+  try {
+    const collection = db.collection("students");
+    const marks = await collection.findOne(
+      { student_id: studentId.trim() },
+      {
+        projection: {
+          student_id: 1,
+          assessment_marks: 1,
+          attendance_marks: 1,
+          linkedin_post_marks: 1,
+          project_review_marks: 1,
+          project_submission_marks: 1,
+          total: {
+            $add: [
+              "$assessment_marks",
+              "$attendance_marks",
+              "$linkedin_post_marks",
+              "$project_review_marks",
+              "$project_submission_marks",
+            ],
+          },
+        },
+      }
+    );
+
+    if (!marks) {
+      return res
+        .status(404)
+        .json({ message: "Marks not found for this student." });
+    }
+
+    res.status(200).json(marks);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error Fetching Users." });
+  } finally {
+    await client.close();
+  }
+}
---
+++ pages/api/upload/attendance.js
@@ -0,0 +1,64 @@
+import { connectToDatabase } from "@/lib/db";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.status(401).json({ message: "Not authorized!" });
+    return;
+  }
+
+  const { data } = req.body;
+
+  if (!data || data.length === 0) {
+    res.status(402).json({ message: "No data found in the file." });
+    return;
+  }
+
+  if (!Array.isArray(data)) {
+    res.status(403).json({ message: "Data must be an array." });
+    return;
+  }
+
+  const trimmedData = [];
+
+  Array.map(data, (item, index) => {
+    trimmedData.push({
+      student_id: item.student_id.trim(),
+      marks: parseFloat(item.marks).toFixed(2),
+    });
+    if (
+      !item.student_id ||
+      !item.marks ||
+      item.student_id.trim().length === 0
+    ) {
+      res
+        .status(424)
+        .json({ message: "Error: Invalid data on line " + (index + 1) + "." });
+      return;
+    }
+  });
+
+  const client = await connectToDatabase();
+  const db = client.db();
+  const collection = db.collection("students");
+
+  try {
+    // Use updateMany to update or insert documents in batch
+    await collection.updateMany(
+      { student_id: { $in: trimmedData.map((item) => item.student_id) } },
+      {
+        $set: trimmedData.map((item) => ({
+          student_id: item.student_id,
+          attendance_marks: item.marks,
+        })),
+      },
+      { upsert: true }
+    );
+
+    res.status(200).json({ message: "Attendance Data uploaded successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error updating data" });
+  } finally {
+    await client.close();
+  }
+}
